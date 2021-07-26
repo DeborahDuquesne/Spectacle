ae45608dc78dc8027fea101828b99022db40a400
Deborah <deborah_duquesne@hotmail.com>
2021-07-27 01:14:09 +0200

    modif fct

---
+++ src/be/condorcet/duquesne/POJO/Organisateur.java
@@ -0,0 +1,73 @@
+package be.condorcet.duquesne.POJO;
+import be.condorcet.duquesne.DAO.AbstractFactoryDAO;
+import be.condorcet.duquesne.DAO.PersonneDAO;
+
+public class Organisateur extends Personne 
+{
+private final AbstractFactoryDAO dao = AbstractFactoryDAO.getFactory(AbstractFactoryDAO.DAO_FACTORY);
+	
+	protected final PersonneDAO pDAO = dao.getPersonneDAO();
+	
+	public final static String statut = "ORGANISTEUR";
+	private String nominationEntreprise;
+	private String numEntreprise;
+	public String getNominationEntreprise() {
+		return nominationEntreprise;
+	}
+	public void setNominationEntreprise(String nominationEntreprise) {
+		this.nominationEntreprise = nominationEntreprise;
+	}
+	public Spectacle getSpect() {
+		return spect;
+	}
+	public void setSpect(Spectacle spect) {
+		this.spect = spect;
+	}
+	public String getNumEntreprise() {
+		return numEntreprise;
+	}
+	public void setNumEntreprise(String numEntreprise) {
+		this.numEntreprise = numEntreprise;
+	}
+	private Spectacle spect;
+	public Organisateur() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Organisateur(int id, String s, String mdp, String statut) {
+		super(id, s, mdp, statut);
+		// TODO Auto-generated constructor stub
+	}
+	public Organisateur(Integer id, String statut, String mdp, String speudo, String adresse, String prenom, String nom,
+			String telephone, String email, int age) {
+		super(id, statut, mdp, speudo, adresse, prenom, nom, telephone, email, age);
+		// TODO Auto-generated constructor stub
+	}
+	public Organisateur(String pseudo, String mdp, String nom, String prenom, 
+			String adresse, int age,String nomination) 
+	{
+		super(pseudo, mdp, nom, prenom, adresse, age);
+		this.nominationEntreprise=nomination;
+		// TODO Auto-generated constructor stub
+	}
+	public Organisateur(String pseudo, String mdp, String statut) {
+		super(pseudo, mdp, statut);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Organisateur(String s, String mdp, String t, String a, String e, String n, String p,String no,String nomm)
+	{
+		super(s, mdp, t, a, e, n, p);
+		this.numEntreprise=no;
+		this.nominationEntreprise=nomm;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean register() 
+	{
+		return pDAO.create(((Organisateur) this));
+	}
+	
+	
+}
