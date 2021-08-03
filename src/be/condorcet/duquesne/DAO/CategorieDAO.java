40d8e89eb108b2e3dca14f58f1b49d1e3916e810
Deborah <deborah_duquesne@hotmail.com>
2021-08-04 01:52:43 +0200

    modif fct

---
+++ src/be/condorcet/duquesne/DAO/CategorieDAO.java
@@ -0,0 +1,93 @@
+package be.condorcet.duquesne.DAO;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import be.condorcet.duquesne.POJO.Categorie;
+import be.condorcet.duquesne.POJO.Configuration;
+import be.condorcet.duquesne.POJO.Categorie.TypesCat;
+
+public class CategorieDAO implements DAO<Categorie> 
+{
+
+	protected Connection connect = null;
+	private Statement stmt=null;
+	
+	public  CategorieDAO(Connection conn) 
+	{
+		connect = conn;
+	}
+	@Override
+	public boolean create(Categorie obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean delete(Categorie obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean update(Categorie obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public Categorie find(Categorie obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public List<Categorie> findAll(Categorie obj) {
+
+		List<Categorie> liste = new ArrayList<Categorie>();
+			Statement stm = null;
+			ResultSet rs = null;
+			try
+			{
+				String sql = "Select * From categorie_ ";
+				//rs = stm.executeQuery(sql);
+				rs=this.connect.createStatement().executeQuery(sql);
+				while(rs.next())
+				{
+					
+					int idCat = Integer.parseInt(rs.getString(1));
+					String com = rs.getString(2);
+					TypesCat typeC= TypesCat.valueOf(rs.getString(3));
+					int prix = (int) Float.parseFloat(rs.getString(4));
+					int nbre = (int) Float.parseFloat(rs.getString(5));
+					int nbre2 = (int) Float.parseFloat(rs.getString(6));
+				// on peut bosser avec le nom des champs aussi c est au choix
+					liste.add(new Categorie(idCat,com,typeC,prix,nbre,nbre2
+							
+							
+							)
+							
+							);		
+					
+				}
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+			return liste;
+	}
+	@Override
+	public List getAll(Categorie object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Categorie findById(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+
---
+++ src/be/condorcet/duquesne/DAO/ReservationDAO.java
@@ -0,0 +1,156 @@
+package be.condorcet.duquesne.DAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import be.condorcet.duquesne.POJO.*;
+
+public class ReservationDAO implements DAO<Reservation> {
+
+	protected Connection con_= null;
+
+	public ReservationDAO(Connection con) {
+		con_ = con;
+	}
+
+	
+	/* INSERT INTO "STUDENT03_27"."RESERVATION_" 
+	 * ("acompte", "solde", "statut", "commentaire", "
+	 * prix", "fk_pers", "fk_planS") VALUES ('150', '500', '
+	 * ENREGISTRE', 'null', '500', '26', '42')
+*/
+	
+	
+	@Override
+	public boolean create(Reservation obj)
+	{
+		try
+        {
+                      
+        	PreparedStatement state = con_.prepareStatement
+        			("INSERT INTO Reservation_(\"acompte\",\"solde\",\"statut\","
+        					+ "\"commentaire\",\"prix\""
+        					+ ",\"fk_pers\",\"fk_planS\")"
+        					
+        					
+        					+ "VALUES (?,?,?,?,?,?,?)");
+        	//Float.parseFloat(rs.getString(9));
+        	state.setFloat(1, obj.getAcompte());
+            state.setFloat(2, obj.getSolde());
+            state.setString(3, obj.getStatut());
+            
+            state.setString(4, obj.getCommentaire());
+      
+            state.setFloat(5, obj.getPrix());
+            state.setInt(6, obj.getOrganisateur().getId());
+            state.setInt(7, obj.getPlanningSalle().getId());
+           
+            
+            state.execute();
+            return true;
+        }
+
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        
+        return false;
+
+	}
+	
+	public List<Representation> findAll_(Representation representation) 
+	{
+		List<Representation> rlist= new ArrayList<Representation>();
+	
+		String sql = "Select * From representation_ inner join Spectacle_"
+				+ " on spectacle_.\"id\"=representation_.\"fk_spect\"";
+		
+		try {
+			ResultSet rs = this.con_.createStatement()					
+					.executeQuery(sql);
+							
+
+			
+			while (rs.next()) 
+			
+			{
+				//le spectacle 
+				Spectacle s = new Spectacle(
+						rs.getInt(7),
+						rs.getString(8),
+						rs.getString(9),
+						rs.getString(10),
+						rs.getString(11),
+						
+						rs.getInt(12)
+						);
+			
+				
+				
+				int rId = Integer.parseInt(rs.getString(1));
+				String date  =rs.getString(2);
+				int heureDebut = (int) Float.parseFloat(rs.getString(3));
+				int heureFin = (int) Float.parseFloat(rs.getString(4));
+				
+				
+				
+				Representation r= new Representation(rId,heureDebut,heureFin, date,s);
+				// ajout de la repre a la liste
+				rlist.add(r);
+									
+				
+						
+			}
+
+		} catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		//JOptionPane.showMessageDialog( null,"taille list represendao findAll avc pk spec  "+rlist.size());
+    	
+		return rlist;
+	}
+	@Override
+	public boolean delete(Reservation obj) {
+		return false;
+	}
+
+	@Override
+	public boolean update(Reservation obj) {
+		return false;
+	}
+
+	
+	@Override
+	public List<Reservation> findAll(Reservation reservation) {
+		return null;
+		
+	}
+
+	@Override
+	public Reservation find(Reservation obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation findById(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getAll(Reservation object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
